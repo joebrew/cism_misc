7a8317b8327b81b0f0e5e61cbe030e5c333dad60
Joe Brew <joebrew@gmail.com>
2016-06-09 10:39:17 +0200

    maps!

--- gadm_example.R
+++ gadm_example.R
@@ -9,9 +9,23 @@ moz1 <- getData('GADM', country = 'MOZ', level =1)
 plot(moz1)
 
 # Level 2
-moz1 <- getData('GADM', country = 'MOZ', level =2)
-plot(moz1)
+moz2 <- getData('GADM', country = 'MOZ', level =2)
+plot(moz2)
 
 # Level 3
-moz1 <- getData('GADM', country = 'MOZ', level =3)
+moz3 <- getData('GADM', country = 'MOZ', level =3)
 plot(moz3)
+plot(moz3, 
+     col = ifelse(moz3@data$NAME_3 == 'Magude', 'red', 'grey'),
+     border = NA)
+
+# Subset for only manhica
+man <- moz3[moz3@data$NAME_2 == 'Manhiça',]
+plot(man)
+
+# Subset for only maputo
+maputo <- moz3[moz3@data$NAME_1 == 'Maputo',]
+
+# Man over moz
+plot(maputo)
+plot(man, col = 'red', add = TRUE)
