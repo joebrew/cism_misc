87584531a6dcfb47b2c948717bd1386f4d99d492
Joe Brew <joebrew@gmail.com>
2016-06-09 10:54:59 +0200

    weather still not great

--- weather_example.R
+++ weather_example.R
@@ -5,3 +5,22 @@ weather <- getWeatherForYear(station_id = 'FQMA',
                              # station_type = 'airportCode',
                              year = 2015,
                              opt_detailed = TRUE)
+
+# Alternative
+# require(devtools)
+# install_github("mpiccirilli/weatheR")
+require(weatheR)
+
+data(stations)
+# or
+moz <- station.list[station.list$CTRY == 'MZ',]
+
+cities <- c("MAPUTO, MZ")
+
+plotStations(cities, station.list, 1)
+
+# Interpolated data
+hourly.data <- getInterpolatedDataByCity(cities, station.list, k = 1, 
+                                         2015, 2015, 100, 24, tolerance= .05)
+
+plotDailyMax(hourly.data$station_data)
